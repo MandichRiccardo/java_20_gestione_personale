992fda5bafb7a7619618745805851d3653759684
MandichRiccardo <ricky.mandich@gmail.com>
2024-01-30 14:10:14 +0100

    aggiornamento 2024 01 30 14:09

---
+++ Staff.java
@@ -0,0 +1,61 @@
+public class Staff{
+    private Lavoratore[] team;
+
+    public Staff(int numeroMembri) {
+        this.team = new Lavoratore[numeroMembri];
+    }
+
+    public Staff() {
+        System.out.println("inserisci il numero dei membri di questo team");
+        team = new Lavoratore[new java.util.Scanner(System.in).nextInt()];
+    }
+
+    public boolean add(Lavoratore l){
+        if(contains(l)) return false;
+        int i=0;
+        while(i<team.length && team[i] != null) i++;
+        if(i<team.length) team[i] = l;
+        return i<team.length;
+    }
+
+    public boolean remove(Lavoratore l){
+        if(!contains(l)) return false;
+        int i=0;
+        while(i<team.length && team[i] != l) i++;
+        team[i] = null;
+        return true;
+    }
+
+    public boolean contains(Lavoratore o){
+        for(Lavoratore l:team){
+            if(l==o) return true;
+        }
+        return false;
+    }
+
+    public double getPaga(int i) {
+        return team[i].calcolaPaga();
+    }
+
+    public double getPagaTotale(){
+        int paga = 0;
+        for(int i=0;i< team.length;i++){
+            try {
+                paga += getPaga(i);
+            }
+            catch (NullPointerException e){
+                paga += 0;
+            }
+        }
+        return paga;
+    }
+
+    public String toString(){
+        String info = "";
+        for(int i=0;i<team.length;i++){
+            info = info.concat("lavoratore " + (i+1) + ":\n" + team[i] + "\n");
+        }
+        info += "membri:\t\t\t" + team.length;
+        return info;
+    }
+}
--- Test.java
+++ Test.java
@@ -1,5 +1,38 @@
 public class Test{
     public static void main(String[] args) {
-
+        Staff s = new Staff();
+        Lavoratore l1 = creaLavoratore(), l2 = creaLavoratore(), l3 = creaLavoratore(), l4 = creaLavoratore(), l5 = creaLavoratore();
+        System.out.println(s);
+        s.add(l1);
+        s.add(l2);
+        s.add(l3);
+        s.add(l4);
+        s.add(l5);
+        System.out.println(s);
+        System.out.println("inserisci il numero relativo al lavoratore di cui vuoi conoscere la paga");
+        System.out.println("la paga è " + s.getPaga(new java.util.Scanner(System.in).nextInt()-1));
+        System.out.println("la paga totale di questo staff è " + s.getPagaTotale());
+    }
+    public static Lavoratore creaLavoratore(){
+        Lavoratore l = null;
+        System.out.println(
+                """
+                    che tipo di lavoratore vuoi creare?
+                        premere 0 per un lavoratore qualsiasi
+                        premere 1 per un volontario
+                        premere 2 per un dipendente
+                        premere 3 per un dipendente giornaliero
+                        premere 4 per un impiegato
+                        """
+        );
+        switch (new java.util.Scanner(System.in).nextInt()) {
+            case 0 -> l = new Lavoratore();
+            case 1 -> l = new Volontario();
+            case 2 -> l = new Dipendente();
+            case 3 -> l = new Giornaliero();
+            case 4 -> l = new Impiegato();
+        }
+        if(l != null) return l;
+        else return creaLavoratore();
     }
 }
